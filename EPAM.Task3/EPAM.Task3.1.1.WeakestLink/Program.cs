ebb8049dc8e3c0ee395cdbda898cc0193beb743c
KlimovKirill <45049287+KlimovKirill@users.noreply.github.com>
2020-07-13 20:00:25 +0300

    Task3.1 -3.2

---
+++ EPAM.Task3/EPAM.Task3.1.1.WeakestLink/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.Task3._1._1.WeakestLink
+{
+    public class Program
+    {
+        public static void SearchOfLast(Queue<int> roundOfPeople, int n)
+        {
+            bool flag = true;
+            int countOfRounds = 1;
+            while (roundOfPeople.Count != 1)
+            {
+
+                if (flag)
+                {
+                    roundOfPeople.Enqueue(roundOfPeople.Dequeue());
+                    flag = !flag;
+                }
+                else
+                {
+                    roundOfPeople.Dequeue();
+                    flag = !flag;
+                    
+                    Console.WriteLine($"Раунд {countOfRounds}. Вычеркнут человек. Людей осталось: {n - countOfRounds}");
+                    countOfRounds++;
+                }
+
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Queue<int> roundOfPeople = new Queue<int>();
+
+                Console.WriteLine("Введите N: ");
+                int.TryParse(Console.ReadLine(), out int n);
+
+                for (int i = 1; i <= n; i++)
+                {
+                    roundOfPeople.Enqueue(i);
+                }
+
+                Console.WriteLine("Сгенерирован круг людей. Начинаем вычеркивать каждого второго.");
+
+                SearchOfLast(roundOfPeople, n);
+                
+                Console.WriteLine($"Игра окончена. Невозможно вычеркнуть больше людей. Последний оставшийся человек под номером: {roundOfPeople.Peek()}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка! Неверные входные данные.");
+            }
+        }
+    }
+}
---
+++ EPAM.Task3/EPAM.Task3.1.2.TextAnalysis/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EPAM.Task3._1._2.TextAnalysis
+{
+    class Program
+    {
+        public static void WordFounder(string s, ref Dictionary<string, int> wordFreq)
+        {
+            s.ToLower();
+            char[] separators = new char[12] { ' ', '.', ',', ';', ':', '?', '!', '-', '(', ')', '\n', '\r' };
+            while (s.Length != 0)
+            {
+                int i_sep = s.IndexOfAny(separators);
+                if (i_sep == -1)
+                {
+                    i_sep = s.Length;
+                }
+
+                if (!wordFreq.ContainsKey(s.Substring(0, i_sep)))
+                {
+                    if (s.Substring(0, i_sep) != " " && !String.IsNullOrEmpty(s.Substring(0, i_sep)))
+                    {
+                        wordFreq.Add(s.Substring(0, i_sep), 1);
+                    }
+                }
+                else
+                {
+                    wordFreq[s.Substring(0, i_sep)]++;
+                }
+
+                if (i_sep >= s.Length)
+                {
+                    i_sep = s.Length - 1;
+                }
+
+                s = s.Remove(0, i_sep + 1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+
+            Console.WriteLine("Hello! Please, enter the path to your file with text you need to check: ");
+            string pathToFile = Console.ReadLine();
+
+            Console.WriteLine(Environment.NewLine);
+            
+            StreamReader sr = new StreamReader(pathToFile);
+            string s = sr.ReadToEnd();
+            
+            WordFounder(s, ref wordFreq);
+
+            Console.WriteLine("Table of words and their frequencies (in descending order):" + Environment.NewLine);
+
+            var items = from pair in wordFreq
+                        orderby pair.Value descending
+                        select pair;
+
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
